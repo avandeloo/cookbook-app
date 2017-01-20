65a8583a765343a6dc01741de71240707c251a9e
avandeloo <anthony.vandeloo@gmail.com>
2017-01-19 21:19:22 -0600

    reformated routes to use index and show and implemented their controller actions and views

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,23 +1,30 @@
 class RecipesController < ApplicationController
-  def one_recipe
-    @recipe = Recipe.last
-  end
+  # def one_recipe
+  #   @recipe = Recipe.last
+  # end
 
-  def all_recipes
+  # def all_recipes
+  #   @recipes = Recipe.all
+  # end
+
+  # def recipe_form
+  # end
+
+  # def recipe_success
+  #   @recipe = Recipe.new(
+  #                         title: params["title"],
+  #                         chef: params["chef"],
+  #                         ingredients: params["ingredients"],
+  #                         directions: params["directions"]
+  #                       )
+  #   @recipe.save
+  # end
+  def index
     @recipes = Recipe.all
   end
 
-  def recipe_form
-  end
-
-  def recipe_success
-    @recipe = Recipe.new(
-                          title: params["title"],
-                          chef: params["chef"],
-                          ingredients: params["ingredients"],
-                          directions: params["directions"]
-                        )
-    @recipe.save
+  def show
+    @recipe = Recipe.find(params[:id])
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
-  get '/one_recipe_url' => 'recipes#one_recipe'
-  get '/all_recipes_url' => 'recipes#all_recipes'
+  # get '/one_recipe_url' => 'recipes#one_recipe'
+  # get '/all_recipes_url' => 'recipes#all_recipes'
 
-  get '/recipe_form' => 'recipes#recipe_form'
-  post '/recipe_success' => 'recipes#recipe_success'
+  # get '/recipe_form' => 'recipes#recipe_form'
+  # post '/recipe_success' => 'recipes#recipe_success'
+
+  get '/' => 'recipes#index'
+  get '/recipes' => 'recipes#index'
+  get 'recipes/:id' => 'recipes#show'
+
 end
