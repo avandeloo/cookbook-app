d21e2e0ebcd9f002db53f4786eb8aebe48d8c1eb
avandeloo <anthony.vandeloo@gmail.com>
2017-01-30 21:16:27 -0600

    added abilities to login and logout as well as signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,4 @@
 Rails.application.routes.draw do
-  # get '/one_recipe_url' => 'recipes#one_recipe'
-  # get '/all_recipes_url' => 'recipes#all_recipes'
-
-  # get '/recipe_form' => 'recipes#recipe_form'
-  # post '/recipe_success' => 'recipes#recipe_success'
 
   get '/' => 'recipes#index'
   get '/recipes' => 'recipes#index'
@@ -18,4 +13,10 @@ Rails.application.routes.draw do
 
   delete '/recipes/:id' => 'recipes#destroy'
 
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
 end
